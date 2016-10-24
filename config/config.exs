af36835513be204c36d9402fc932b96a169ef6ce
Harry Grumbar <harry@goodlord.co>
2016-10-24 17:32:25 +0100

    Prepare for CI

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,14 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :todos, Pheddit.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  username: System.get_env("DB_USER") || "phoenix",
+  password: System.get_env("DB_PASSWORD") || "",
+  database: System.get_env("DB_DATABASE") || "todos_test",
+  hostname: System.get_env("DB_HOST") || "127.0.0.1",
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "8")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
