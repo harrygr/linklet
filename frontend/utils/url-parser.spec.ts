d339440c7fa67db720d0d4b04079e9afc3935a9d
Harry Grumbar <harry@goodlord.co>
2017-01-13 11:31:41 +0000

    Update url parser test

--- frontend/utils/url-parser.spec.ts
+++ frontend/utils/url-parser.spec.ts
@@ -1,14 +1,14 @@
 import {expect} from 'chai'
 import urlParser from './url-parser'
 
-describe('A test', function () {
-  it('tests the truth', function () {
+describe('The URL parser', function () {
+  it('extracts the domain from a full url', function () {
 
     const urls = {
       'http://www.youtube.com/watch?v=ClkQA2Lb_iE': 'www.youtube.com',
       'http://youtu.be/ClkQA2Lb_iE': 'youtu.be',
       'http://www.example.com/12xy45': 'www.example.com',
-      'http://example.com/random': 'example.com',
+      'https://example.com/random': 'example.com',
       'example.com/random': 'example.com',
       'example.com:8000/random': 'example.com',
       'example.com?q=hello': 'example.com',
