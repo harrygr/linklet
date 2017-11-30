6e51064e748abe92102e1d9af0665edb400a6aef
harrygr <harry@grumbar.me>
2017-11-30 22:51:15 +0000

    Persist state to localstorage

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -6,6 +6,8 @@ import { State as UiState } from './ui/reducer'
 import { State as LinksState } from './links/reducer'
 import { State as AuthState } from './auth/reducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { saveState, retrieveState } from './localStorage'
+import { debounce } from 'lodash'
 
 export interface State {
   ui: UiState
@@ -15,7 +17,20 @@ export interface State {
 
 const store = createStore(
   reducer,
+  retrieveState(),
   composeWithDevTools(applyMiddleware(thunkMiddleware)),
+)
+
+store.subscribe(
+  debounce(() => {
+    const state = store.getState()
+    if (state) {
+      saveState({
+        auth: state.auth,
+        links: state.links,
+      })
+    }
+  }, 1000),
 )
 
 export default store
