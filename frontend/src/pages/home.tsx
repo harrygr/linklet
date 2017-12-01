63a7f5a9313ee74dc11b44c05f4b585ea456dd03
Harry Grumbar <harry@goodlord.co>
2017-12-01 10:52:21 +0000

    Force loading links

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,26 +2,27 @@ import * as React from 'react'
 import LinkList from '../link-list'
 import { connect, Dispatch } from 'react-redux'
 import { State } from '../store'
-import { fetchLinksIfNeeded } from '../store/effects'
+import { fetchLinksIfNeeded, fetchLinks } from '../store/effects'
 
 interface StateMappedToProps {}
 interface DispatchMappedToProps {
-  loadLinks: () => void
+  fetchLinks: () => void
+  fetchLinksIfRequired: () => void
 }
 
 interface Props extends StateMappedToProps, DispatchMappedToProps {}
 
 export class Home extends React.Component<Props> {
   componentDidMount() {
-    this.props.loadLinks()
+    this.props.fetchLinksIfRequired()
   }
   render() {
-    const { loadLinks } = this.props
+    const { fetchLinks } = this.props
     return (
       <div>
         <h1>Home</h1>
         <p>Welcome</p>
-        <button onClick={loadLinks}>Load links</button>
+        <button onClick={fetchLinks}>Load links</button>
         <LinkList />
       </div>
     )
@@ -33,7 +34,10 @@ function mapStateToProps(s: State) {
 }
 
 function mapDispatchToProps(dispatch: Dispatch<any>) {
-  return { loadLinks: () => dispatch(fetchLinksIfNeeded()) }
+  return {
+    fetchLinks: () => dispatch(fetchLinks()),
+    fetchLinksIfRequired: () => dispatch(fetchLinksIfNeeded()),
+  }
 }
 
 export default connect<StateMappedToProps, DispatchMappedToProps>(
