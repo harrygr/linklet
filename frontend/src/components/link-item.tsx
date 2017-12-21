01e80481c6ab8d4d6c57c9badbf0dc1bf38e2fa7
Harry Grumbar <harry@goodlord.co>
2017-12-21 17:24:18 +0000

    Break out link summary into own component

---
+++ frontend/src/components/link-item.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import styled, { css } from 'react-emotion'
+import { Option } from 'catling'
+import { colors, spacing } from '../styles'
+import { Link, CreateVote } from '../api/types'
+import { LinkHeading, LinkMeta, ArrowDown, ArrowUp, ButtonLink } from './'
+
+interface Props {
+  link: Link
+  onVote: (vote: CreateVote) => any
+  userId: Option<number>
+}
+
+const ScoreContainer = styled('div')`
+  font-size: 25px;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${colors.grey};
+`
+
+const arrowContainerStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 45px;
+  width: 17px;
+  margin-right: ${spacing.s2};
+`
+
+const arrowStyle = (voted: boolean) => css`
+  fill: ${voted ? colors.grey : colors.greyLight};
+`
+
+export function LinkItem({ link, onVote, userId }: Props) {
+  const vote = userId
+    .flatMap(id => Option(link.votes.find(v => v.user_id === id)))
+    .map(v => v.direction)
+    .getOrElse(0)
+
+  const score = link.votes.reduce((sum, v) => sum + v.direction, 0)
+
+  const upvoteChange = vote === 1 ? 0 : 1
+  const downvoteChange = vote === -1 ? 0 : -1
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <ScoreContainer>{score}</ScoreContainer>
+      <div className={arrowContainerStyle}>
+        <ButtonLink
+          onClick={() =>
+            onVote({
+              direction: upvoteChange,
+              link_id: link.id,
+            })
+          }
+        >
+          <ArrowUp className={arrowStyle(vote === 1)} />
+        </ButtonLink>
+        <ButtonLink
+          onClick={() =>
+            onVote({
+              direction: downvoteChange,
+              link_id: link.id,
+            })
+          }
+        >
+          <ArrowDown className={arrowStyle(vote === -1)} />
+        </ButtonLink>
+      </div>
+      <div>
+        <LinkHeading url={link.url} title={link.title} />
+        <LinkMeta
+          username={link.user.username}
+          linkDate={link.inserted_at}
+          linkId={link.id}
+          commentCount={link.comments_count}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default LinkItem
--- frontend/src/components/link-list.tsx
+++ frontend/src/components/link-list.tsx
@@ -1,17 +1,7 @@
 import * as React from 'react'
-import styled, { css } from 'react-emotion'
 import { Option } from 'catling'
-import { colors, spacing } from '../styles'
 import { Link, CreateVote } from '../api/types'
-import {
-  ListItem,
-  LinkHeading,
-  List,
-  LinkMeta,
-  ArrowDown,
-  ArrowUp,
-  ButtonLink,
-} from './'
+import { LinkItem, List, ListItem } from './'
 
 interface Props {
   links: Link[]
@@ -19,82 +9,14 @@ interface Props {
   userId: Option<number>
 }
 
-const ScoreContainer = styled('div')`
-  font-size: 25px;
-  width: 45px;
-  height: 45px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${colors.grey};
-`
-
-const arrowContainerStyle = css`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 45px;
-  width: 17px;
-  margin-right: ${spacing.s2};
-`
-
-const arrowStyle = (voted: boolean) => css`
-  fill: ${voted ? colors.grey : colors.greyLight};
-`
-
 export function LinkList({ links, onVote, userId }: Props) {
   return (
     <List>
-      {links.map(link => {
-        const vote = userId
-          .flatMap(id => Option(link.votes.find(v => v.user_id === id)))
-          .map(v => v.direction)
-          .getOrElse(0)
-
-        const score = link.votes.reduce((sum, v) => sum + v.direction, 0)
-
-        const upvoteChange = vote === 1 ? 0 : 1
-        const downvoteChange = vote === -1 ? 0 : -1
-
-        return (
-          <ListItem key={link.id}>
-            <div style={{ display: 'flex' }}>
-              <ScoreContainer>{score}</ScoreContainer>
-              <div className={arrowContainerStyle}>
-                <ButtonLink
-                  onClick={() =>
-                    onVote({
-                      direction: upvoteChange,
-                      link_id: link.id,
-                    })
-                  }
-                >
-                  <ArrowUp className={arrowStyle(vote === 1)} />
-                </ButtonLink>
-                <ButtonLink
-                  onClick={() =>
-                    onVote({
-                      direction: downvoteChange,
-                      link_id: link.id,
-                    })
-                  }
-                >
-                  <ArrowDown className={arrowStyle(vote === -1)} />
-                </ButtonLink>
-              </div>
-              <div>
-                <LinkHeading url={link.url} title={link.title} />
-                <LinkMeta
-                  username={link.user.username}
-                  linkDate={link.inserted_at}
-                  linkId={link.id}
-                  commentCount={link.comments_count}
-                />
-              </div>
-            </div>
-          </ListItem>
-        )
-      })}
+      {links.map((link, i) => (
+        <ListItem key={link.id}>
+          <LinkItem key={i} link={link} onVote={onVote} userId={userId} />
+        </ListItem>
+      ))}
     </List>
   )
 }
