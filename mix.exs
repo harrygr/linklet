9de6cc9b38cc6eccc3e7d4312acce3f80f9ed9cd
harrygr <harry@grumbar.me>
2018-04-28 11:09:09 +0100

    Use mysql 5 in CI

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Linklet.Mixfile do
     [{:phoenix, "~> 1.3.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
-     {:mariaex, ">= 0.8.2"},
+     {:mariaex, ">= 0.8.3"},
      {:phoenix_html, "~> 2.10"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
